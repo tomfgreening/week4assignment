488ae02bfcbb97ad15558aa8d4fc32137cf2d3e4
Thomas Greening <tomfgreening@gmail.com>
2025-01-10 19:00:29 +0000

    Added some pseudo-code to server file and added the database URI to the env file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-import express from "express"
+import express from "express";
 const app = express();
-// I am importing EXPRESS into my server file on line 1 and 'turning it on' with line 2. 
+// I am importing EXPRESS into my server file on line 1 and 'turning it on' with line 2.
 
 import cors from "cors";
 app.use(cors());
@@ -10,7 +10,15 @@ app.use(express.json());
 // I am telling my EXPRESS server that I want it to use JSON).
 
 app.listen(8080, () => {
-    console.log('Express server is running on port 8080');
+  console.log("Express server is running on port 8080");
 });
+// I am running my EXPRESS server in local host 8080.
 
+app.get("/", (req, res) => {
+  res.json({ message: "This is the route of the root!" });
+});
+// I am getting the data from the end point / at local port 8080 and displaying it.
 
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config()
