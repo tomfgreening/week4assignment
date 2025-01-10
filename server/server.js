5058715f4e8b3f6942a320bc0217390e6f48cb5f
Thomas Greening <tomfgreening@gmail.com>
2025-01-10 14:24:12 +0000

    Adding the functionality to my server.js file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,15 @@
 import express from "express"
 const app = express();
 // I am importing EXPRESS into my server file on line 1 and 'turning it on' with line 2. 
+
+import cors from "cors";
+app.use(cors());
+// Same principle before but with CORS this time.
+
+app.use(express.json());
+// I am telling my EXPRESS server that I want it to use JSON).
+
+app.listen(8080, () => {
+    console.log('Express server is running on port 8080');
+});
+
