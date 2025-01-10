f0c6c9f558126ea33e8213a836541faade20c204
Thomas Greening <tomfgreening@gmail.com>
2025-01-10 13:50:45 +0000

    pushing from main project file for good practice

--- server/server.js
+++ server/server.js
@@ -1,3 +1,3 @@
 import express from "express"
 const app = express();
-// I am importing express into my server file on line 1 and 'turning it on' on line 2. 
+// I am importing express into my server file on line 1 and 'turning it on' with line 2. 
