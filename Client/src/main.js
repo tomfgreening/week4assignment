0a1c274a6b7a467d760bf089d1b8d8e92d3e3059
Thomas Greening <tomfgreening@gmail.com>
2025-01-12 16:11:45 +0000

    Completed form

--- Client/src/main.js
+++ Client/src/main.js
@@ -1,15 +1,16 @@
-const messageForm = document.querySelector("#messageForm")
+const messageForm = document.querySelector("#messageForm");
 
-function  handleSubmitMessageForm(event) {
-event.preventDefault();
-const formData =  new FormData(messageForm)
-const formValues = Object.fromEntries(formData)
-fetch("http://localhost:8080/messages",{
-  method: "POST"
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({formValues})
-});
+function handleSubmitMessageForm(event) {
+  event.preventDefault();
+  const formData = new FormData(messageForm);
+  const formValues = Object.fromEntries(formData);
+  fetch("http://localhost:8080/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ formValues }),
+  });
+  console.log(formValues);
 }
 messageForm.addEventListener("submit", handleSubmitMessageForm);
